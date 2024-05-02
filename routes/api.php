d9fccc04a5888be3864fd01ae65d74770dc11338
Zaki Lazuardi Ferysa Putra <zakilfp987@gmail.com>
2024-05-02 23:35:22 +0700

    Praktikum 3 JS 10 API 1

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use App\Http\Controllers\Api\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\LevelController;
+use App\Http\Controllers\Api\BarangController;
+use App\Http\Controllers\Api\KategoriController;
+use App\Http\Controllers\Api\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +21,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
 Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
+Route::middleware('auth:api')->get('/user', function(Request $request){
     return $request->user();
 });
+
+Route::get('levels', [LevelController::class, 'index']);
+Route::post('levels', [LevelController::class, 'store']);
+Route:: get('levels/{level}', [LevelController :: class, 'show']);
+Route::put('levels/{level}', [LevelController::class, 'update']);
+Route:: delete('levels/{level}', [LevelController::class, 'destroy']);
