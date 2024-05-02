b197ea228b8266f8bdf9abec75bba01f0d21374a
Zaki Lazuardi Ferysa Putra <zakilfp987@gmail.com>
2024-05-02 20:27:32 +0700

    Praktikum 1 JS 10 API 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,24 +5,25 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Foundation\Auth\User;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class UserModel extends User
+class UserModel extends Authenticatable implements JWTSubject
 {
-    use HasFactory;
+    public function getJWTIdentifier(){
+        return $this->getKey();
+    }
 
-    protected $table = "m_user";
-    public $timestamps = false;
-    protected $primaryKey = 'user_id';
+    public function getJWTCustomClaims(){
+        return[];
+    }
+    
+    protected $table = 'm_user'; //mendefinisikan nama tabel yang digunakan oleh model ini
+    protected $primaryKey = 'user_id'; //medefinisikan primary key dari tabel yang digunakan
+    protected $fillable = ['level_id', 'username', 'nama', 'password'];
 
-    protected $fillable = [
-        'user_id',
-        'level_id',
-        'username',
-        'nama',
-        'password',
-    ];
-    public function level(): BelongsTo {
+    public function level(): BelongsTo{
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
 }
